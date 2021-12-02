f831ed55983fd1b1fdb7247c3fe28e1b0b262f87
nitin <kunitin@vmware.com>
2021-12-02 13:54:30 +0530

    Reading properties file

--- SampleSBApplication/src/main/java/com/nitin/SampleSBApplication/SampleSbApplication.java
+++ SampleSBApplication/src/main/java/com/nitin/SampleSBApplication/SampleSbApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableScheduling
 public class SampleSbApplication implements CommandLineRunner {
@@ -15,6 +20,8 @@ public class SampleSbApplication implements CommandLineRunner {
     private static Logger logger = LoggerFactory.getLogger(SampleSbApplication.class);
     @Value("${person.name:defaultString}")
     private String name;
+    @Value("${custom.prop.name:notFoundCustomPropName}")
+    private String customName;
 
     public static void main(String[] args) {
         logger.info("This is info log");
@@ -31,6 +38,21 @@ public class SampleSbApplication implements CommandLineRunner {
             System.out.println("arg: " + arg);
         }
         System.out.println("Name: " + name);
+    }
+
+    @PostConstruct
+    public void loadPropertiesPostInit(){
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("customFolder/custom.properties"));
+            //properties.load(new FileReader("src/main/resources/customFolder/custom.properties")); //this also works
+            logger.info("** loaded custom.properties. Now reading ..");
+            logger.info("** Property custom.prop.name={}",properties.getProperty("custom.prop.name"));
+            logger.info(customName); //prints notFoundCustomPropName
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
     }
 }
 
